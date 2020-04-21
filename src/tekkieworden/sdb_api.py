61125d4cf90e27102656af595278333bc25e89c2
rmania <dmeijerink1979@gmail.com>
2020-04-21 17:57:31 +0200

    wip

--- src/tekkieworden/sdb_api.py
+++ src/tekkieworden/sdb_api.py
@@ -1,9 +1,37 @@
 """
 https://api.studiekeuzedatabase.nl/Studiekeuzedatabase.svc/
 """
-
+from rauth import OAuth2Service
 import requests
 from typing import Dict
+
+from dotenv import dotenv_values
+
+
+class ExampleOAuth2Client:
+    def __init__(self, client_id, client_secret):
+        self.access_token = None
+
+        self.service = OAuth2Service(
+            name="studiekeuze_api",
+            client_id=dotenv_values()['CLIENT_ID'],
+            client_secret=dotenv_values()['CLIENT_SECRET'],
+            access_token_url="https://token.studiekeuzedatabase.nl/token",
+            authorize_url="https://token.studiekeuzedatabase.nl/token",
+            base_url="https://api.studiekeuzedatabase.nl/Studiekeuzedatabase.svc/",
+        )
+
+        self.get_access_token()
+
+    def get_access_token(self):
+        data = {'code': 'bar',
+                'grant_type': 'client_credentials',
+                'redirect_uri': 'http://example.com/'}
+
+        session = self.service.get_auth_session(data=data, decoder=json.loads)
+
+        self.access_token = session.access_token
+
 
 class SDB:
     def __init__(self, token: str):
