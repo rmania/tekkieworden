e59d38780a585bc78980d954f2ec5613ebec0167
rmania <dmeijerink1979@gmail.com>
2020-04-29 23:01:39 +0200

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -54,5 +54,17 @@ setup(
     extras_require={},
     include_package_data=True,
     license='BSD 3',
-    python_requires="==3.6.*",
+    python_requires="==3.7.*",
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
