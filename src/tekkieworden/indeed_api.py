265f6b232af64d25e7cc2363bb0099f163b1ff9f
rmania <dmeijerink1979@gmail.com>
2020-04-30 23:25:30 +0200

    remove old stuff

--- src/tekkieworden/indeed_api.py
+++
@@ -1,2 +0,0 @@
-# https://medium.com/@alberto_moura/build-a-jobs-database-using-indeeds-api-8f95316be842
-# https://github.com/rowandl/portfolio/blob/master/Webscraping%20Indeed.com/Webscraping%20Indeed.ipynb
